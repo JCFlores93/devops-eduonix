959d602967fec06e60073293ba082f5176b6546a
admin <admin@MacBook-Pro-de-admin.local>
2020-01-10 17:27:08 -0500

    first commit

---
+++ devops-foundation/final_project/product_inventory.py
@@ -0,0 +1,70 @@
+from json import dumps, loads
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def to_dict(self):
+        return {"name": self.name, "price": self.price}
+
+    def print_description(self):
+        print("Product ({}) - Price: ${}".format(self.name, self.price))
+
+def total_products(products):
+    total = 0.0
+    for product in products:
+        total += product.price
+    print("Your products hava a total value of: {} \n\n".format(total))
+
+def load_products():
+    try:
+        products_file = open("products.json", "r+")
+    except IOError:
+        return []
+    product_json = products_file.read()
+    product_data = loads(product_json)
+
+    products = []
+    for product in product_data:
+        products.append(Product(product["name"], product["price"]))
+    products_file.close()
+    return products
+
+def add_product(name, price):
+    new_product = Product(name, price)
+    products.append(new_product)
+
+def list_products(products):
+    for p in products:
+       p.print_description()
+
+def save_products(products):
+    products_save_list = []
+    for product in products:
+        products_save_list.append(product.to_dict())
+    products_file = open("products.json", "w+")
+    products_file.write(dumps(products_save_list))
+    products_file.close()
+
+products = load_products()
+while True:
+    print("Type 'add' to add a product")
+    print("Type 'quit' to quite the program")
+    print("Type 'list' to list all the products")
+    print("Type 'total' to show the total sum of your products")
+    command = input("type a command")
+    if command == "quit":
+        save_products(products)
+        break
+    if command == "add":
+        product_name = input("enter a name for your product")
+        try:
+            product_price = float(input("enter a price for your product"))
+        except ValueError:
+            print("Enter a valid price")
+            continue
+        add_product(product_name, product_price)
+    if command == "list":
+        list_products(products)
+    if command == "total":
+        total_products(products)
